8e624e237a712268412e16e1e0f6e463e02ea128
snahmd <sanahmed7@gmail.com>
2024-07-31 11:36:18 +0200

    add film in html

--- src/movie/movie.ts
+++ src/movie/movie.ts
@@ -2,3 +2,29 @@ import { movies } from "./data";
 
 console.log("Movie", movies);
 console.log("hvbcjeh");
+
+const movie = movies[0];
+
+const container = document.getElementById("container");
+
+// container.innerHTML = `
+// <h1>${movie[0]}</h1>
+// <h1>${movie[1]}</h1>
+// <h1>${movie[2]}</h1>
+// <h1>${movie[3]}</h1>
+// <h1>${movie[4]}</h1>
+// <h1>${movie[5]}</h1>
+// `;
+
+for (let i = 0; i < movies.length; i++) {
+  const movie = movies[i];
+  container.innerHTML += `
+  <h1>${movie[0]}</h1>
+  <h1>${movie[1]}</h1>
+  <h1>${movie[2]}</h1>
+  <h1>${movie[3]}</h1>
+  <h1>${movie[4]}</h1>
+  <h1>${movie[5]}</h1>
+  <hr>
+  `;
+}
