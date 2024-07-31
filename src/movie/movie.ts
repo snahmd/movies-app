17f70c20658944db7132c7471de520b760ac4593
snahmd <sanahmed7@gmail.com>
2024-07-31 12:22:51 +0200

    add for loop movie in html

--- src/movie/movie.ts
+++ src/movie/movie.ts
@@ -3,9 +3,10 @@ import { movies } from "./data";
 console.log("Movie", movies);
 console.log("hvbcjeh");
 
-const movie = movies[0];
+// const movie = movies[0];
 
 const container = document.getElementById("container");
+const movieContainer = document.getElementById("movieContainer");
 
 // container.innerHTML = `
 // <h1>${movie[0]}</h1>
@@ -18,13 +19,27 @@ const container = document.getElementById("container");
 
 for (let i = 0; i < movies.length; i++) {
   const movie = movies[i];
-  container.innerHTML += `
-  <h1>${movie[0]}</h1>
-  <h1>${movie[1]}</h1>
-  <h1>${movie[2]}</h1>
-  <h1>${movie[3]}</h1>
-  <h1>${movie[4]}</h1>
-  <h1>${movie[5]}</h1>
-  <hr>
-  `;
+  const ersteDiv = document.createElement("div");
+  const ersteH = document.createElement("h1");
+  const zweiteH = document.createElement("h1");
+  const dritteH = document.createElement("h1");
+  const vierteH = document.createElement("h1");
+  const funfteH = document.createElement("h1");
+  const sechsteH = document.createElement("h1");
+
+  ersteH.textContent += "Title: " + movie[0];
+  zweiteH.textContent += "Year: " + movie[1];
+  dritteH.textContent += "Regisseur:in: " + movie[2];
+  vierteH.textContent += "Spieldauer: " + movie[3];
+  funfteH.textContent += "Genre: " + movie[4];
+  sechsteH.textContent += "Bewertung: " + movie[5];
+  ersteDiv.style.border = "1px solid black";
+  console.log(ersteH);
+  ersteDiv.appendChild(ersteH);
+  ersteDiv.appendChild(zweiteH);
+  ersteDiv.appendChild(dritteH);
+  ersteDiv.appendChild(vierteH);
+  ersteDiv.appendChild(funfteH);
+  ersteDiv.appendChild(sechsteH);
+  movieContainer.appendChild(ersteDiv);
 }
