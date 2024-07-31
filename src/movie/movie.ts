c30eb10fc305e9bd41a8744450b10e5766c67765
snahmd <sanahmed7@gmail.com>
2024-07-31 14:27:34 +0200

    add sort method for year up

--- src/movie/movie.ts
+++ src/movie/movie.ts
@@ -17,29 +17,44 @@ const movieContainer = document.getElementById("movieContainer");
 // <h1>${movie[5]}</h1>
 // `;
 
-for (let i = 0; i < movies.length; i++) {
-  const movie = movies[i];
-  const ersteDiv = document.createElement("div");
-  const ersteH = document.createElement("h1");
-  const zweiteH = document.createElement("h1");
-  const dritteH = document.createElement("h1");
-  const vierteH = document.createElement("h1");
-  const funfteH = document.createElement("h1");
-  const sechsteH = document.createElement("h1");
+// sort movies
+const sortYearUp = document
+  .getElementById("yearup")
+  .addEventListener("click", function () {
+    movies.sort((a, b) => {
+      return Number(a[1]) - Number(b[1]);
+    });
+    createMovieElement(movies);
+  });
 
-  ersteH.textContent += "Title: " + movie[0];
-  zweiteH.textContent += "Year: " + movie[1];
-  dritteH.textContent += "Regisseur:in: " + movie[2];
-  vierteH.textContent += "Spieldauer: " + movie[3];
-  funfteH.textContent += "Genre: " + movie[4];
-  sechsteH.textContent += "Bewertung: " + movie[5];
-  ersteDiv.style.border = "1px solid black";
-  console.log(ersteH);
-  ersteDiv.appendChild(ersteH);
-  ersteDiv.appendChild(zweiteH);
-  ersteDiv.appendChild(dritteH);
-  ersteDiv.appendChild(vierteH);
-  ersteDiv.appendChild(funfteH);
-  ersteDiv.appendChild(sechsteH);
-  movieContainer.appendChild(ersteDiv);
+function createMovieElement(movies) {
+  movieContainer.innerHTML = "";
+  for (let i = 0; i < movies.length; i++) {
+    const movie = movies[i];
+    const ersteDiv = document.createElement("div");
+    const ersteH = document.createElement("h1");
+    const zweiteH = document.createElement("h1");
+    const dritteH = document.createElement("h1");
+    const vierteH = document.createElement("h1");
+    const funfteH = document.createElement("h1");
+    const sechsteH = document.createElement("h1");
+
+    ersteH.textContent += "Title: " + movie[0];
+
+    zweiteH.textContent += "Year: " + movie[1];
+    dritteH.textContent += "Regisseur:in: " + movie[2];
+    vierteH.textContent += "Spieldauer: " + movie[3];
+    funfteH.textContent += "Genre: " + movie[4];
+    sechsteH.textContent += "Bewertung: " + movie[5];
+    ersteDiv.style.border = "1px solid black";
+    console.log(ersteH);
+    ersteDiv.appendChild(ersteH);
+    ersteDiv.appendChild(zweiteH);
+    ersteDiv.appendChild(dritteH);
+    ersteDiv.appendChild(vierteH);
+    ersteDiv.appendChild(funfteH);
+    ersteDiv.appendChild(sechsteH);
+    movieContainer.appendChild(ersteDiv);
+  }
 }
+createMovieElement(movies);
